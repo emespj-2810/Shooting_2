9b6f1b4a0c17a6390a54de245f6190b99f8a9ab2
hw16a125 <hw16a125@oecu.jp>
2017-11-28 15:43:26 +0900

    エラーを修正しました。　HW16A125　鶴田朋己

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -31,21 +31,10 @@ void Start()
 // 1/60秒ごとに呼ばれる関数です。モデルの更新と画面の描画を行います。
 void Update()
 {
-    //大砲の上下移動
-    if (barrel == true) {
-        cannonPos.y += 1;
-        if (cannonPos.y > -70) {
-            barrel = false;
-        }
-    }
-    if (barrel == false) {
-        cannonPos.y -= 1;
-        if (cannonPos.y < -145) {
-            barrel = true;
-        }
     // 弾の発射
     if (bulletPos.x <= -999 && Input::GetKeyDown(KeyMask::Space)) {
         bulletPos = cannonPos + Vector2(50, 10);
+        PlaySound("se_maoudamashii_explosion03.mp3");
     }
 
     // 弾の移動
@@ -58,6 +47,7 @@ void Update()
             score += 1;         // スコアの加算
             bulletPos.x = -999; // 弾を発射可能な状態に戻す
         }
+        PlaySound("se_maoudamashii_explosion06.mp3");
     }
 
     // 背景の描画
